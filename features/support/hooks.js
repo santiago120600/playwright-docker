b235b4e2ae245cbb04a477c57f9abaf39c8190ca
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2024-11-05 20:33:06 -0600

    save video on failure

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,18 +1,27 @@
-const { Before, After, AfterStep, Status } = require('@cucumber/cucumber');
+const { Before, BeforeStep, After, AfterStep, Status } = require('@cucumber/cucumber');
 const { chromium } = require('@playwright/test');
 const POManager = require('../../pageobjects/POManager');
 const path = require('path');
 const fs = require('fs');
 
-Before(async function () {
-    browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext();
-    this.page = await context.newPage();
+let stepName;
+let scenarioName;
+
+Before(async function (scenario) {
+    this.browser = await chromium.launch({ headless: false });
+    this.context = await this.browser.newContext();
+    await this.context.tracing.start({ screenshots: true, snapshots: true });
+    this.page = await this.context.newPage();
     this.poManager = new POManager(this.page);
+    scenarioName = scenario.pickle.name;
+});
+
+BeforeStep(async (scenario) => {
+    stepName = scenario.pickleStep.text;
 });
 
 After(async function () {
-    await browser.close();
+    await this.browser.close();
 });
 
 AfterStep(async function ({ result, pickleStep }) {
@@ -24,5 +33,8 @@ AfterStep(async function ({ result, pickleStep }) {
         const screenshotName = pickleStep.text.replace(/[^a-z0-9]/gi, '_').toLowerCase() + new Date().toISOString().replace(/[-:T.Z]/g, '');
         const screenshotFilePath = path.join(screenshotPath, `${screenshotName}.png`);
         await this.page.screenshot({ path: screenshotFilePath });
+        const videoPath = path.resolve("test-results/videos");
+        const videoFilePath = path.join(videoPath, `${scenarioName}.zip`);
+        await this.context.tracing.stop({ path: videoFilePath });
     }
 });
